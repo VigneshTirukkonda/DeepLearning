64179f0abfe6237aa9068ec2d7eec2dd2463d8c9
VigneshTirukkonda <vigneshtj2@gmail.com>
2021-02-16 22:55:51 +0530

    flask app

--- MNIST/output/app.py
+++ MNIST/output/app.py
@@ -1,43 +1,44 @@
-import io
+from flask import Flask, render_template, url_for, request
+import json
+from flask_jsglue import JSGlue
+import config
 import model
 import torch
-import config
-import torchvision.transforms as transforms
-from PIL import Image
-from flask import Flask, render_template, url_for, request, jsonify
 
-
-
-
-def transform_img(image_bytes):
-    transform = transforms.Compose([
-        transforms.Resize([28, 28]),
-        transforms.ToTensor(),
-        transforms.Lambda(lambda x: torch.unsqueeze(x, 0)),
-        transforms.Normalize((0.5,), (0.5))
-    ])
-    image = Image.open(io.BytesIO(image_bytes)).convert('L')
-    return transform(image)
-
-def get_prediction(image_bytes):
-    tensor = transform_img(image_bytes)
-    outputs = Model.forward(tensor)
-    return outputs.tolist()
-
-
-app = Flask(__name__)
 Model = model.CNN()
 Model.load_state_dict(torch.load(config.MODEL_PATH + config.LATEST_MODEL))
-Model.eval()
+
+app = Flask(__name__)
+jsglue = JSGlue(app)
+
+def transform_image(image_bytes):
+    my_transforms = transforms.Compose([
+        transforms.Resize((28, 28)),
+        transforms.ToTensor(),
+        transforms.Lambda(lambda x: torch.unsqueeze(x, 0)),
+        transforms.Normalize([0.5, ],[0.5, ])
+        ])
+    image = Image.open(io.BytesIO(image_bytes))
+    return my_transforms(image)
+
+
+def get_prediction(image_bytes):
+    tensor = transform_image(image_bytes=image_bytes)
+    outputs = Model.forward(tensor)
+    return outputs.tolist()
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        file = request.files['file']
-        img_bytes = file.read()
-        predictions = get_prediction(img_bytes)
-    return jsonify({'class_probs': predictions})
+        # file = request.files['file']
+        # img_bytes = file.read()
+        img_bytes = json.loads(request.data['file'])
+        # 2
 
-if __name__ == "__main__":
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
